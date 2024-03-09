480ad3bb90edaf326c6c4557d756bd715bee191d
Kirill <kirill2004pro1000@mail.ru>
2024-03-10 00:32:30 +1000

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Component } from "react";
+import ReactDOM from "react-dom/client";
 
+interface Param {
+  id: number;
+  name: string;
+  type: "string";
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+
+interface Color {
+  id: number;
+  value: string;
+}
+
+interface Model {
+  paramValues: ParamValue[];
+  colors: Color[];
+}
+
+interface Props {
+  params: Param[];
+  model: Model;
+}
+
+interface State {
+  paramValues: ParamValue[];
+}
+
+class ParamEditor extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      paramValues: props.model.paramValues.slice(),
+    };
+  }
+
+  handleChange = (paramId: number, value: string) => {
+    const updatedParamValues = this.state.paramValues.map((paramValue) =>
+      paramValue.paramId === paramId ? { ...paramValue, value } : paramValue
+    );
+    this.setState({ paramValues: updatedParamValues });
+  };
+
+  getModel = (): Model => {
+    return {
+      paramValues: this.state.paramValues,
+      colors: this.props.model.colors,
+    };
+  };
+
+  render() {
+    const { params } = this.props;
+    const { paramValues } = this.state;
+
+    return (
+      <div>
+        {params.map((param) => (
+          <div key={param.id}>
+            <label>{param.name}</label>
+            <input
+              type="text"
+              value={
+                paramValues.find((p) => p.paramId === param.id)?.value || ""
+              }
+              onChange={(e) => this.handleChange(param.id, e.target.value)}
+            />
+          </div>
+        ))}
+        <button onClick={() => console.log(this.getModel())}>getModel</button>
+      </div>
+    );
+  }
+}
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const model: Model = {
+  colors: [],
+  paramValues: [
+    {
+      paramId: 1,
+      value: "повседневное",
+    },
+    {
+      paramId: 2,
+      value: "макси",
+    },
+  ],
+};
+
+const params: Param[] = [
+  {
+    id: 1,
+    name: "Назначение",
+    type: "string",
+  },
+  {
+    id: 2,
+    name: "Длина",
+    type: "string",
+  },
+];
+
+root.render(<ParamEditor model={model} params={params} />);
